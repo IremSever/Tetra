f27d2d5dc8059974d01f2789219b559e669a9eae
Irem Sever <99093113+IremSever@users.noreply.github.com>
2023-09-11 11:41:59 +0300

    up

---
+++ Assets/ForceUpdate.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+public class ForceUpdate : MonoBehaviour
+{
+    public GameObject versionPanel;
+    string version;
+    void Start()
+    {
+        using (var req = new HttpClient())
+        {
+            var response = req.GetAsync("https://api.tmgrup.com.tr/config/471");
+            var jsonText = response.Result.Content.ReadAsStringAsync().Result;
+            JObject jo = JObject.Parse(jsonText);
+            version = jo.SelectToken("data").SelectToken("tetris").SelectToken("androidversion").ToString();
+        }
+        string myVersion = Application.version.ToString();
+        Debug.Log(myVersion);
+        Debug.Log(version + "api");
+        if (version != myVersion)
+        {
+            Debug.Log("Version Control");
+            versionPanel.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+    public void getTolink()
+    {
+        string url = "https://www.google.com";
+        Application.OpenURL(url);
+    }
+}
