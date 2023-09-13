9a80df699c8ed7e6e71de8c6502ed40123db1e9e
Irem Sever <99093113+IremSever@users.noreply.github.com>
2023-09-13 14:48:42 +0300

    up

--- Assets/ForceUpdate.cs
+++ Assets/ForceUpdate.cs
@@ -1,3 +1,39 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.UI;
+//using Newtonsoft.Json;
+//using Newtonsoft.Json.Linq;
+//using System.Net.Http;
+//public class ForceUpdate : MonoBehaviour
+//{
+//    [SerializeField] GameObject versionPanel;
+//    string version;
+//    void Start()
+//    {
+//        using (var req = new HttpClient())
+//        {
+//            var response = req.GetAsync("https://api.tmgrup.com.tr/config/471");
+//            var jsonText = response.Result.Content.ReadAsStringAsync().Result;
+//            JObject jo = JObject.Parse(jsonText);
+//            version = jo.SelectToken("data").SelectToken("tetris").SelectToken("androidversion").ToString();
+//        }
+//        string myVersion = Application.version.ToString();
+//        Debug.Log(myVersion);
+//        Debug.Log(version + "api");
+//        if (version != myVersion)
+//        {
+//            Debug.Log("Version Control");
+//            versionPanel.SetActive(true);
+//            Time.timeScale = 0;
+//        }
+//    }
+//    public void getTolink()
+//    {
+//        string url = "https://www.google.com";
+//        Application.OpenURL(url);
+//    }
+//}
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,9 +45,9 @@ public class ForceUpdate : MonoBehaviour
 {
     [SerializeField] GameObject versionPanel;
     string version;
+    string myVersion;
     void Start()
     {
-        versionPanel.SetActive(false);
         using (var req = new HttpClient())
         {
             var response = req.GetAsync("https://api.tmgrup.com.tr/config/471");
@@ -19,7 +55,7 @@ public class ForceUpdate : MonoBehaviour
             JObject jo = JObject.Parse(jsonText);
             version = jo.SelectToken("data").SelectToken("tetris").SelectToken("androidversion").ToString();
         }
-        string myVersion = Application.version.ToString();
+        myVersion = Application.version.ToString();
         Debug.Log(myVersion);
         Debug.Log(version + "api");
         if (version != myVersion)
@@ -33,5 +69,10 @@ public class ForceUpdate : MonoBehaviour
     {
         string url = "https://www.google.com";
         Application.OpenURL(url);
+        if (version == myVersion)
+        {
+            versionPanel.SetActive(false);
+            Time.timeScale = 1;
+        }
     }
 }
