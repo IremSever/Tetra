dc7950364ce196382659addf05e43725f2256771
Irem Sever <99093113+IremSever@users.noreply.github.com>
2023-10-02 10:01:16 +0300

    up

--- Assets/Scripts/Managers/ForceUpdate.cs
+++ Assets/Scripts/Managers/ForceUpdate.cs
@@ -1,39 +1,4 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//using UnityEngine.UI;
-//using Newtonsoft.Json;
-//using Newtonsoft.Json.Linq;
-//using System.Net.Http;
-//public class ForceUpdate : MonoBehaviour
-//{
-//    [SerializeField] GameObject versionPanel;
-//    string version;
-//    void Start()
-//    {
-//        using (var req = new HttpClient())
-//        {
-//            var response = req.GetAsync("https://api.tmgrup.com.tr/config/471");
-//            var jsonText = response.Result.Content.ReadAsStringAsync().Result;
-//            JObject jo = JObject.Parse(jsonText);
-//            version = jo.SelectToken("data").SelectToken("tetris").SelectToken("androidversion").ToString();
-//        }
-//        string myVersion = Application.version.ToString();
-//        Debug.Log(myVersion);
-//        Debug.Log(version + "api");
-//        if (version != myVersion)
-//        {
-//            Debug.Log("Version Control");
-//            versionPanel.SetActive(true);
-//            Time.timeScale = 0;
-//        }
-//    }
-//    public void getTolink()
-//    {
-//        string url = "https://www.google.com";
-//        Application.OpenURL(url);
-//    }
-//}
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
